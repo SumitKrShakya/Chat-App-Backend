3389081ca535d2ba3d649eb4aa2e376ea558549e
Sumit Kumar Shakya <sumitshakya20101999@gmail.com>
2022-09-25 15:14:41 +0530

    cors second update

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: process.env.PORT,
+        origin: 'https://skschat.netlify.app/',
         credentials: true,
     }
 })
